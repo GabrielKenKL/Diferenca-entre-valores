751705536f8071ffe20e2d06337ed371c96971a4
Gabriel Ken Kudo de Lima <gabkenkudo@gmail.com>
2026-03-02 11:40:01 -0300

    Primeira versão dos códigos.

---
+++ Cálculo da diferença.py
@@ -0,0 +1,13 @@
+print("Cálculo da diferença entre dois números reais:")
+print("digite dois números para efetuar a operação:")
+
+print("número 1:")
+x = float(input())
+
+print("número 2:")
+y = float(input())
+
+#Calculando a diferença entre os números:
+operacao = x - y
+
+print("A diferença entre os números equivale a:" , operacao)
